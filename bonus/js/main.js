4a9d2e41761a12fcafa6378856897f61ed3142cc
ChrisGarozzo <garozzochristian@gmail.com>
2023-11-28 19:35:52 +0100

    added difficult levels on js

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -1,9 +1,22 @@
 const grid = document.querySelector(".grid");
 const btn_play = document.getElementById("btn-play");
+let squareElement;
 btn_play.addEventListener("click",
-    function () {
-        for (let i = 1; i <= 100; i++) {
-            const squareElement = createMyElement("div", "square");
+function () {
+        let selectDiff = parseInt(document.getElementById("selectDiff").value);
+        grid.innerHTML = "";
+        for (let i = 1; i <= lunghezza(selectDiff); i++) {
+            console.log(lunghezza(selectDiff));
+            if(lunghezza(selectDiff) === 100){
+                squareElement = createMyElement("div","square");
+            }
+            else if(lunghezza(selectDiff) === 81){
+                squareElement = createMyElement("div","squareNormal");
+            }
+            else if(lunghezza(selectDiff) === 49){
+                squareElement = createMyElement("div","squareHard");
+            }
+
             squareElement.append(i);
             squareElement.addEventListener("click",
                 function () {
@@ -13,9 +26,22 @@ btn_play.addEventListener("click",
             grid.append(squareElement);
         }
     }
-)
+);
 
-function createMyElement(tagName, className) {
+function lunghezza(diff){
+    let lung;
+    if(diff === 0){
+        lung = 100;
+    } else if (diff === 1){
+        lung = 81;
+    }
+    else if(diff === 2){
+        lung = 49;
+    }
+    return lung;
+}
+
+function createMyElement(tagName,className) {
     const currentElement = document.createElement(tagName);
     currentElement.classList.add(className);
     return currentElement;
