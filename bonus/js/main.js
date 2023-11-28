d0c52e6051d8d5677e69eb85dc17147cf29257c2
ChrisGarozzo <garozzochristian@gmail.com>
2023-11-28 17:51:50 +0100

    added bonus forder

---
+++ bonus/js/main.js
@@ -0,0 +1,24 @@
+const grid = document.querySelector(".grid");
+const btn_play = document.getElementById("btn-play");
+btn_play.addEventListener("click",
+    function () {
+        for (let i = 1; i <= 100; i++) {
+            const squareElement = createMyElement("div", "square");
+            squareElement.append(i);
+            squareElement.addEventListener("click",
+                function () {
+                    this.classList.add("clicked");
+                }
+            )
+            grid.append(squareElement);
+        }
+    }
+)
+
+function createMyElement(tagName, className) {
+    const currentElement = document.createElement(tagName);
+    currentElement.classList.add(className);
+    return currentElement;
+}
+
+
